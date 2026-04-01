d9d530223d9e082cb3667df79ca7a46e9f8e95cc
Zaaaza13 <63474590+Ilkhomjon13@users.noreply.github.com>
2026-04-01 14:57:08 +0500

    Release 1.2.17 hotfix: always show update button

--- scripts/build-velopack.ps1
+++ scripts/build-velopack.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Version = "1.2.16",
+    [string]$Version = "1.2.17",
     [string]$Runtime = "win-x64",
     [string]$Channel = "stable",
     [string]$OutputDir = "ReleaseBundles\velopack"
